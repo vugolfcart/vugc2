98aa9b6504c50dc7dc01bd60d2e9f9f465353094
Kenneth Li <kenkraken@gmail.com>
2020-02-26 19:22:41 -0600

    Update steering_pid with driveservice

--- vugc2_control/src/steering_pid.py
+++ vugc2_control/src/steering_pid.py
@@ -2,6 +2,7 @@
 
 import rospy
 from vugc2_control.msg import Drive_param, Torque_param, Angle_param
+from vugc2_control.srv import DriveService, DriveServiceRequest, DriveServiceResponse
 from std_msgs.msg import Bool
 from sensor_msgs.msg import Image
 from numpy import interp
@@ -18,51 +19,50 @@ cte = 0
 prev_cte = 0
 acc_cte = 0
 
-voltage_maximum_difference = 1.5
-voltage_center = 2.5
-voltage_low = voltage_center - (voltage_maximum_difference / 2.0)
-voltage_high = voltage_center + (voltage_maximum_difference / 2.0)
-steering_low = -100
-steering_high = 100
-torque_low = 4095 / 5.0 * voltage_low
-torque_high = 4095 / 5.0 * voltage_high
-
-
-def on_drive_parameters(data):
-    global desired_angle
-    desired_angle = data.angle
+DESIRED_ANGLE = 0
+PERCEIVED_ANGLE = 0
 
 def on_angle_parameters(data):
-    print("ANGLE CALLBACK")
-    print("Desired angle: ", desired_angle)
+    global PERCEIVED_ANGLE
+    PERCEIVED_ANGLE = data.angle
+
+def drive(driveParams):
+
+    rospy.wait_for_service('vugc2_control_drive_service')
+    try:
+        driveService = rospy.ServiceProxy('vugc2_control_drive_service', DriveService)
+        torqueParams = driveService(driveParams)
+
+        return torqueParams.torque_params
+    except rospy.ServiceException:
+        print('[vugc2_control_drive_service] service call failed')
+        return None
+
+def on_drive_parameters(data):
+    global DESIRED_ANGLE
+    DESIRED_ANGLE = data.angle
+    print("Desired angle: ", DESIRED_ANGLE)
     print("Perceived angle: ", data.angle)
 
-    global perceived_angle
-    perceived_angle = data.angle
+    # TODO: Arduino doesnt seem to read back angle changes anymore
 
     global cte, prev_cte, acc_cte
-    cte = perceived_angle - desired_angle
+    cte = PERCEIVED_ANGLE - DESIRED_ANGLE
     diff_cte = cte - prev_cte
     prev_cte = cte
     acc_cte += cte
 
-    voltage_difference = -(kp * cte + ki * acc_cte + kd * diff_cte)
-    print("PID controlval: ", voltage_difference)
-    # clamp
-    voltage_difference = interp(voltage_difference, [-1 * voltage_maximum_difference, voltage_maximum_difference], [-1 * voltage_maximum_difference, voltage_maximum_difference])
-    voltage_1 = voltage_center + (voltage_difference / 2.0)
-    voltage_2 = voltage_center - (voltage_difference / 2.0)
+    target = -(kp * cte + ki * acc_cte + kd * diff_cte)
 
-    # volts to torque
-    torque_1 = int(interp(voltage_1, [voltage_low, voltage_high], [torque_low, torque_high]))
-    torque_2 = int(interp(voltage_2, [voltage_low, voltage_high], [torque_low, torque_high]))
+    print("PID virtual target angle: ", PERCEIVED_ANGLE + target)
 
-    print('(kp, ki, kd)={}, volts={}, torque={}'.format((kp, ki, kd), (voltage_1, voltage_2), (torque_1, torque_2)))
+    print('(kp, ki, kd)={}'.format((kp, ki, kd)))
 
-    parameters = Torque_param()
-    parameters.torque1 = torque_1
-    parameters.torque2 = torque_2
-    control_torque_parameters.publish(parameters)
+    driveParams = Drive_param(target, 0) # 0 velocity 
+
+    torqueParams = drive(DriveServiceRequest(driveParams))
+
+    control_torque_parameters.publish(torqueParams)
 
 
 def main():
